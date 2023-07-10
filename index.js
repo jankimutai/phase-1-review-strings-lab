4fae3f26d1754f74a36cfcd05a6dc9bac07e990a
Kimutai <jankimsingoei@gmail.com>
2023-07-10 22:14:49 +0300

    task done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Kimutai Jan';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
